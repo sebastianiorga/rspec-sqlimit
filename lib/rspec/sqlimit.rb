aa9b3dc128425e30d5a87570a1c908a483740794
Sebastian Iorga <sebastianiorga@users.noreply.github.com>
2023-02-22 16:57:26 +0000

    Update sqlimit.rb

--- lib/rspec/sqlimit.rb
+++ lib/rspec/sqlimit.rb
@@ -7,6 +7,45 @@ module RSpec
     require_relative "sqlimit/counter"
     require_relative "sqlimit/reporter"
   end
+  
+  Matchers.define :trigger_num_queries do |expected|
+    chain :with do |matcher|
+      @matcher = matcher
+    end
+
+    match do |block|
+      disabled_cache_block = -> { ::ActiveRecord::Base.uncached { block.call } }
+      @counter ||= RSpec::SQLimit::Counter[@matcher, disabled_cache_block]
+      @counter.count == expected
+    end
+
+    match_when_negated do |block|
+      disabled_cache_block = -> { ::ActiveRecord::Base.uncached { block.call } }
+      @counter ||= RSpec::SQLimit::Counter[@matcher, disabled_cache_block]
+      @counter.count != expected
+    end
+
+    failure_message do |_|
+      message(expected, @counter)
+    end
+
+    failure_message_when_negated do |_|
+      message(expected, @counter, true)
+    end
+
+    supports_block_expectations
+
+    def message(expected, counter, negation = false)
+      reporter    = RSpec::SQLimit::Reporter.new(counter)
+      condition   = negation ? 'not to' : 'to'
+      restriction = " that match #{reporter.matcher}" if reporter.matcher
+
+      <<-MESSAGE.gsub(/ +\|/, '')
+        |Expected #{condition} run exactly #{expected} queries#{restriction}
+        |#{reporter.call}
+      MESSAGE
+    end
+  end
 
   Matchers.define :exceed_query_limit do |expected|
     chain :with do |matcher|
